4c7111436c405c0f19e593a761f2867ed9851145
zulune <mars.xaker@gmail.com>
2018-05-08 17:21:36 +0300

    create __init__ settings

---
+++ flaskr/__init__.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask 
+
+
+def create_app():
+	app = Flask(__name__)
+	app.config.from_mapping(
+		SECRET_KEY='dev',
+		DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+	)
+
+	if test_config == None:
+		app.config.from_pyfile('config.py', silent=True)
+	else:
+		app.config.from_mapping(test_config)
+
+	try:
+		os.makedirs(app.instance_path)
+	except OSError:
+		pass
+
+
+	@app.route('/hello')
+	def hello():
+		return 'Hello, world!'
+
+	return app
